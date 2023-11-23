37e7ef6a38d7203d575b828b05494eb2d0025569
Youssef Hergal <youssefher247@gmail.com>
2023-11-24 00:33:35 +0100

    Ajout de quelque fonctionaliteés (delete , download , notification)

--- static/main.js
+++ static/main.js
@@ -8,3 +8,25 @@ document.getElementById('fileUpload').addEventListener('change', function (e) {
         reader.readAsText(file);
     }
 });
+
+function downloadJson() {
+    var jsonData = document.getElementById('jsonData').value;
+
+    if (jsonData.trim() === '') {
+        $('#emptyOutputModal').modal('show');
+    } else {
+        var blob = new Blob([jsonData], { type: 'application/json' });
+        var url = window.URL.createObjectURL(blob);
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = 'converted.json';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+    }
+}
+
+function deleteInputs() {
+    document.getElementById('xmlContent').value = '';
+    document.getElementById('jsonData').value = '';
+}
